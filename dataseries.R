07a16b26d0a679100ceaa822a4cf25b54572bf98
Johannes <jbracher@outlook.de>
2018-11-12 00:16:11 +0100

    Slight update of dataseries.R

--- dataseries.R
+++ dataseries.R
@@ -1,4 +1,5 @@
 install.packages("dataseries")
+library(dataseries)
 
 # Hole Wechselkurse des CHF mit Euro, US-Dollar, Pfund (Codes auf http://www.dataseries.org/)
 wechselkurse <- dataseries::ds(c("FXR.M0.USD1", "FXR.M0.EUR1", "FXR.M0.GBP1"))
@@ -9,4 +10,3 @@ colnames(wechselkurse) <- c("Zeit", "USD", "EUR", "GBP")
 plot(wechselkurse$Zeit, wechselkurse$EUR,
      xlab = "Zeit", ylab = "Franken pro Fremdwährung", # Achsenbeschriftungen
      type = "l") # zeichne Linie anstelle von Punkten
-plot(loess(USD, data = wechselkurse))
